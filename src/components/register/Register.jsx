49ae45312fb1b3e52960f95d3ee9588fc1fff6bb
Jimmy Bonilla <jimmyb091202@gmail.com>
2023-12-01 13:44:47 -0800

    m

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -30,6 +30,8 @@ const Register = () => {
             setError({...error, warn: false})
             fetch(`${session.SERVER_URL}/register`, {
                 method: 'POST',
+                mode: 'cors',
+                credentials: 'include',
                 headers: {
                     'Content-Type': 'application/json',
                 },
